43aede9b482879a1ef47980e0b0ba518963d40a1
Samir Djaafer <samirdjaafer@Samirs-MacBook-Air.local>
2023-11-26 13:06:50 +0000

    included prework web page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const outputTopics = function () {
+  const topics = ["Git", "GitHub", "HTML", "CSS", "JavaScript"];
+
+  for (x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+};
+
+outputTopics();
